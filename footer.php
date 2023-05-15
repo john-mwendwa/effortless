44efa059ef922dac90f6201f988920ca47dc5792
RuthlessCoderMJ <72203185+ruthlesscodermj@users.noreply.github.com>
2023-05-15 18:48:50 +0300

    Website edits (#3)
    * Edits in Effortless Website
    
    I changed the About, Footer, Header, and Index files with minor text changes. Additionally, I commented on / unlinked the codes we spoke about at Friday's meeting.
    
    The only thing I couldn't change were photos but that can be solved on Monday
    
    * Effortless Website Changes
    
    New Changes Based On Call With Titus:
    1. Changed the phone number on the website
    2. Removed the Senteu plaza location - to remain only with Gigiri's
    3. The "More Videos" linking to his youtube page is removed
    
    Other Changes For the Future:
    1. The images to be changed would be done after further discussions with Titus on Tuesday

--- footer.php
+++ footer.php
@@ -12,8 +12,8 @@
 						</div>
 						<!-- Updated the location -->
 						<div class="media-body">
-						United Nations Crescent, Gigiri, Nairobi Kenya	
-						Senteu Plaza, Lenana, Nairobi Kenya
+
+						United Nations Crescent, Gigiri, Nairobi Kenya
 						</div>
 					</div>
 					<div class="media thin-media">
@@ -21,7 +21,7 @@
 							<i class="fa fa-phone"></i>
 						</div>
 						<div class="media-body">
-							+254 733 266638 
+							+254 718 727587 
 						</div>
 					</div>
 					<div class="media thin-media">
@@ -42,6 +42,7 @@
 					</div>
 				</div>
 			</div>
+
 <!-- Unlinking the Newsletter section -->
 		<!--	<div class="col-md-6 col-sm-6">
 				<div class="widget widget_mailchimp">
@@ -51,23 +52,23 @@
 					<p>Subscribe to our latest news to be updated, we promise not to spam!</p> -->
 
 					<!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+<!-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 	#mc_embed_signup{background:transparent; clear:left; font:14px Helvetica,Arial,sans-serif; }
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
+</style> -->
+<!-- <div id="mc_embed_signup">
 <form action="https://effortlessinsightafrica.us18.list-manage.com/subscribe/post?u=4841089b6b9d0fe24c788955f&amp;id=8c1e028c3b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
+    <div id="mc_embed_signup_scroll"> -->
 	
-	<input type="email" value="" name="EMAIL" class="email form-control pad-input" id="mce-EMAIL" placeholder="Email address" required>
+	<!-- <input type="email" value="" name="EMAIL" class="email form-control pad-input" id="mce-EMAIL" placeholder="Email address" required> -->
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4841089b6b9d0fe24c788955f_8c1e028c3b" tabindex="-1" value=""></div>
+    <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4841089b6b9d0fe24c788955f_8c1e028c3b" tabindex="-1" value=""></div>
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button theme_button"></div>
     </div>
 </form>
-</div>
+</div> -->
 
 <!--End mc_embed_signup-->
 
@@ -82,7 +83,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center">
-				<p class="lato small-text">Effortless Insight Africa &copy; copyright 2019 all rights reserved</p>
+				<p class="lato small-text">Effortless Insight Africa &copy; copyright 2023 all rights reserved</p>
 			</div>
 		</div>
 	</div>
